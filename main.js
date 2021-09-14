1190a01b49c8a99d0796fbc98d9bc051a8346c85
Magali Mas <magga.mas@hotmail.com>
2021-09-14 19:29:38 -0300

    desafioTres

--- main.js
+++ main.js
@@ -1,3 +1,101 @@
+// DESAFIO 3
+
+//PRIMER EJEMPLO CON FOR (SORTEO IPHONE)
+
+let ingresarCodigo = parseInt(prompt('ingresa el numero para ver si te ganaste el iphone! Tienes un intento'));
+
+for (let i = 1; i <= 150; i++) {
+    if (ingresarCodigo == 73) {
+    alert('¡Felicitaciones! Te has ganado el iphone');
+    break;
+  }
+  console.log('Usuario ingreso' + ': ' + 'número ' + ingresarCodigo);
+}
+
+//OTRO EJEMPLO CON FOR (DIFERENCIA EDADES)
+
+let edadHijo = parseInt(prompt('Quieres saber cuantos años tenía tu madre cuando te tuvo? Bien, primero, ingresa tu edad'));
+let edadPadre = parseInt(prompt('Ahora ingrese edad de su madre'));
+
+for (let edad = 10; edad <=105; edad++) {
+    let edadFinal = edadPadre - edadHijo;
+    alert('Tu madre tenía ' + edadFinal + ' años');
+    break;
+}
+
+//OTRO EJEMPLO CON FOR (TURNO)
+
+let fecha = parseInt(prompt('Ingrese la fecha de hoy. Ejemplo: 6'));
+const ultimoDia = 30;
+
+for (let desde = 1; desde <= ultimoDia; desde++) {
+    let ingreseTurno = prompt('Turnos disponibles hasta el 30 de septiembre. Introduzca que día quiere el turno (ejemplo: 15)');
+    let diasRestantes = ingreseTurno - fecha;
+
+    console.log('Faltan ' + diasRestantes + ' dias para el turno');
+    break;
+}
+
+
+// EJEMPLO WHILE
+
+let creador = prompt('Introduzca el nombre de la creadora de este sitio');
+
+while (creador != 'no se') {
+    if (creador == 'Magali') {
+        let apellido = prompt('Ahora introduzca el apellido');
+        switch (apellido) {
+            case 'Mas':
+                alert('Correcto. Bienvenido');
+                break;
+    
+            case 'MAS':
+                alert('Solo se aceptan mayúsculas en la primer letra.'); 
+                break;
+                
+            default:
+                alert('Incorrecto');
+                break;
+        }
+    } else {
+        alert('Acceso denegado. Posible robot.');
+        break;
+    }
+
+    apellido = prompt('Última chance. Introduzca el apellido');
+    break;
+    console.log(apellido);
+}
+
+
+// OTRO EJEMPLO PARA MI 
+
+/* let pais = prompt("Indique en que país habita");
+
+while(pais != '' ) {
+    console.log('Pais ingresado ' + ': ' + pais);
+    let horario = prompt('Ingrese horario. Ejemplo 16:44');
+    console.log(horario);
+    alert('Actualmente estas en ' + pais + ' y son aproximadamente las ' + horario);
+    break;
+} */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 // DESAFIO 2 
 
 let ingreso = prompt('Para ingresar, introduzca palabra clave');
@@ -39,14 +137,15 @@ if (pregunta != 2021) {
 
 
 
- //DESAFIO 1 
+//DESAFIO 1 
 
-//let nombre = prompt ('Escriba su nombre');
-//alert('Bienvenido' + " " + nombre);
-//let fav = prompt ('Introduzca su numero favorito');
-//let numero = parseInt(fav);
-//let diez = 10;
-//console.log(numero + diez);
-//let pais = prompt ('Introduzca su pais');
-//let barrio = prompt ('Introduzca su barrio');
-//alert('Tu vives en ' + pais + ", " + barrio); 
+let nombre = prompt ('Escriba su nombre');
+alert('Bienvenido' + " " + nombre);
+let fav = prompt ('Introduzca su numero favorito');
+let numero = parseInt(fav);
+let diez = 10;
+console.log(numero + diez);
+let pais = prompt ('Introduzca su pais');
+let barrio = prompt ('Introduzca su barrio');
+alert('Tu vives en ' + pais + ", " + barrio); 
+*/
